88cd21a3fa615d5b151e446a1ae057df62a4e43e
Jose Garcia <josegarciamanez@gmail.com>
2018-04-16 18:01:49 +0200

    app terminada

--- 03-bases-node/app.js
+++ 03-bases-node/app.js
@@ -1,10 +1,43 @@
-const { crearArchivo } = require('./multiplicar/multiplicar');
+const colors = require('colors');
+const argv = require('yargs')
+    .command('listar', 'Imprime en consola la tabla de multiplicar', {
+        base: {
+            demand: true,
+            alias: 'b'
+        },
+        limite: {
+            alias: 'l',
+            default: 10
+        }
+    })
+    .command('crear', 'Genera un archivo con la tabla de multiplicar', {
+        base: {
+            demand: true,
+            alias: 'b'
+        },
+        limite: {
+            alias: 'l',
+            default: 10
+        }
+    })
+    .help()
+    .argv;
+const { crearArchivo, listarTabla } = require('./multiplicar/multiplicar');
 
-let argv = process.argv;
-let parametro = argv[2];
-let base = parametro.split('=')[1];
+let comando = argv._[0];
+switch (comando) {
+    case 'listar':
+        listarTabla(argv.base, argv.limite);
+        break;
+    case 'crear':
+        crearArchivo(argv.base, argv.limite)
+            .then(archivo => console.log(`Archivo creado:` +
+                ` ${archivo}`.green))
+            .catch(e => console.log(e));
+        break;
+    default:
+        console.log('Comando no reconocido');
+}
 
-
-crearArchivo(base)
-    .then(archivo => console.log(`Archivo creado: ${archivo}`))
-    .catch(e => console.log(e));
+// let parametro = argv[2];
+// let base = parametro.split('=')[1];
--- 03-bases-node/multiplicar/multiplicar.js
+++ 03-bases-node/multiplicar/multiplicar.js
@@ -1,6 +1,16 @@
 const fs = require('fs')
+const colors = require('colors');
 
-let crearArchivo = (base) => {
+let listarTabla = (base, limite = 10) => {
+    console.log('========================='.green);
+    console.log(`Tabla del ${base} con límite ${limite}`.red);
+    console.log('========================='.green);
+    for (let mult = 0; mult <= limite; mult++) {
+        console.log(`${base} * ${mult} = ${base * mult}`)
+    }
+}
+
+let crearArchivo = (base, limite = 10) => {
     return new Promise((resolve, reject) => {
         if (!Number(base)) {
             reject(`El valor introducido ${base}, no es un número`);
@@ -8,19 +18,20 @@ let crearArchivo = (base) => {
         }
         let data = ''
 
-        for (let mult = 0; mult <= 10; mult++) {
+        for (let mult = 0; mult <= limite; mult++) {
             data += `${base} * ${mult} = ${base * mult}\n`
         }
 
-        fs.writeFile(`./tablas/tabla-${base}.txt`, data, (err) => {
+        fs.writeFile(`./tablas/tabla-${base}-limite-${limite}.txt`, data, (err) => {
             if (err)
                 reject(err)
             else
-                resolve(`tabla-${base}.txt`)
+                resolve(`tabla-${base}-limite-${limite}.txt`)
         })
     })
 }
 
 module.exports = {
-    crearArchivo
+    crearArchivo,
+    listarTabla
 }
